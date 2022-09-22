dc156f5af1b956e380069603e5c352320ebdb6e4
nkashyap <nkashyap.anand@gmail.com>
2022-09-22 10:49:45 +0530

    22 sep 22

---
+++ POTD/IPL 2021 - Match Day 2.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k)
+    {
+
+        deque<int> dq;
+        vector<int> ans;
+        for (int i = 0; i < n; i++)
+        {
+            while (!dq.empty() && dq.front() <= i - k)
+            {
+                dq.pop_front();
+            }
+            while (!dq.empty() && arr[dq.back()] <= arr[i])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if (i >= k - 1)
+            {
+                ans.push_back(arr[dq.front()]);
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        Solution ob;
+        vector<int> res = ob.max_of_subarrays(arr, n, k);
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
