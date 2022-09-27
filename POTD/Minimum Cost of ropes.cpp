62706a58f286cc1f2d1786c70a8518ab412f6782
nkashyap <nkashyap.anand@gmail.com>
2022-09-27 10:25:26 +0530

    27 sep 22

---
+++ POTD/Minimum Cost of ropes.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+        long long minCost(long long arr[], long long n)
+    {
+        priority_queue<long long, vector<long long>, greater<long long>> pq; // creating min heap
+
+        for (int i = 0; i < n; i++)
+            pq.push(arr[i]);
+
+        long long sum = 0;
+        while (pq.size() > 1)
+        {
+            long long first = pq.top();
+            pq.pop();
+            long long second = pq.top();
+            pq.pop();
+            pq.push(first + second);
+            sum += (first + second);
+        }
+        return sum;
+    }
+};
+
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        long long i, a[n];
+        for (i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        Solution ob;
+        cout << ob.minCost(a, n) << endl;
+    }
+    return 0;
+}
